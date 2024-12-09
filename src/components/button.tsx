3970e5ca3ba64a9097d1b74484535f908423ff50
1CEs <isongwut.me@gmail.com>
2024-12-09 22:20:42 +0700

    chore: tailwind theme configure

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,13 +7,13 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 const Button: React.FC<ButtonProps> = ({ children, isBordered, ...props }) => {
     return (
         <button
-            className={`${isBordered ? 'border-black': 'border-[#d4d0c7]'} border`}
+            className={`${isBordered ? 'border-black': 'border-primary'} border`}
             {...props}
         >
             <div
-                className='w-full h-full border-r-[#adaaa3] border-b-[#adaaa3]
-                           border-l-[#eee] border-t-[#eee] border-2 bg-[#d4d0c7] py-1 px-2
-                           active:bg-[#bcb9b1]'
+                className='w-full h-full border-r-primary-100 border-b-primary-100
+                           border-l-white-100 border-t-white-100 border-2 bg-primary py-1 px-2
+                           active:bg-primary-active'
             >
                 {children}
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      "white": {
+          DEFAULT: "#FFFFFF",
+          100: "#EEEEEE"
+      },
+      "black": "000",
+      "primary": {
+          DEFAULT: "#d4d0c7",
+          100: "#adaaa3",
+          "active": "#bcb9b1"
+      },
+    }
   },
   plugins: [],
 }
